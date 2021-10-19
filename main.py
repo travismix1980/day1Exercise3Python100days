41b5dcce5137d2a6d9d80d24e7d2c395d597fe14
Travis A Mix <36829787+travismix1980@users.noreply.github.com>
2021-10-19 00:15:22 +0000

    We now print the name and number of characters it has

--- main.py
+++ main.py
@@ -2,4 +2,9 @@
 
 # get user name
 name = input("What is your name? ")
+
+# print user name
 print(name)
+
+# print username and number of characters in it
+print("Your name " + name + " has " + str(len(name)) + " characters")
